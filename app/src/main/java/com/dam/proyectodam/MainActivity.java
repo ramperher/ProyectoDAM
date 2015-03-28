dc2d1b192d981acdc0ca0685dda160d4f7f55e5a
ramperher <ramperher@alum.us.es>
2015-03-28 21:52:22 +0100

    Fin definitivo de pruebas

--- app/src/main/java/com/dam/proyectodam/MainActivity.java
+++ app/src/main/java/com/dam/proyectodam/MainActivity.java
@@ -1,13 +1,13 @@
 /* TRABAJO DISEÑO DE APLICACIONES MÓVILES
  *
- *   MainActivity.java
+ * MainActivity.java
  *
- *   Por:
- *      -Ramón Pérez Hernández
- *      -Alberto Rodríguez Blázquez
+ * Por:
+ *  -Ramón Pérez Hernández
+ *  -Alberto Rodríguez Blázquez
  *
- *   Link del repositorio (GitHub):
- *      https://github.com/ramperher/ProyectoDAM
+ * Link del repositorio (GitHub):
+ *  https://github.com/ramperher/ProyectoDAM
  */
 
 package com.dam.proyectodam;
