240c2199d16d79ea6dbf9324fd825a49831d9d76
Alberto Rodríguez <albertoroblaz@hotmail.com>
2015-05-11 13:14:45 +0200

    Añadidos metodos del GPS en actividad CalculationActivity y la clase BBDD.

---
+++ app/src/main/java/com/dam/proyectodam/BBDD.java
@@ -0,0 +1,176 @@
+package com.dam.proyectodam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.location.Location;
+import android.util.Log;
+
+
+/**
+ * Created by Alberto on 08/05/2015.
+ */
+public class BBDD extends SQLiteOpenHelper {
+
+    private static final int VERSION_BBDD=1;
+    private static final String NOMBRE_BBDD="gps.db";
+    private static final String TABLA_LOCALIZACION="CREATE TABLE IF NOT EXIST posiciones " +
+            "(id INTEGER PRIMARY KEY, latitud TEXT, longitud TEXT, velocidad TEXT, tiempo TEXT)";
+    private static final String DROP_LOCALIZACIONES="drop table if exist posiciones";
+    private static int indiceMaximo=100;         //Numero de puntos con lo que trabajara la aplicacion.
+    private static boolean sobreescribir=false;  //Indica que se ha superado el numero de pustos maximos en la BD.
+    private static int id=0;                     //Indice de la base de datos.
+    private static String sinVelocidad="0";      //Usado cuando no se dispone de la velocidad de movimiento.
+
+    public BBDD (Context context){
+        super(context, NOMBRE_BBDD, null, VERSION_BBDD);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLA_LOCALIZACION);
+        Log.d("BBDD", "Creacion de la BBDD()");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DROP_LOCALIZACIONES);
+        db.execSQL(TABLA_LOCALIZACION);
+    }
+
+    public boolean insertarPosicion(Location posicion) {
+        long salida=0;
+        SQLiteDatabase db = getWritableDatabase();
+        if (db != null) {
+            ContentValues valores = new ContentValues();
+            valores.put("_id", id);
+            valores.put("latitud", posicion.convert(posicion.getLatitude(),Location.FORMAT_DEGREES));    //Como la BD no acepta Double, se guardaran como TEXT.
+            valores.put("longitud", posicion.convert(posicion.getLongitude(), Location.FORMAT_DEGREES));
+            valores.put("tiempo", posicion.convert(posicion.getTime(), Location.FORMAT_SECONDS));
+            if(posicion.hasSpeed()){
+                valores.put("velocidad", String.valueOf(posicion.getSpeed()));
+            } else {
+                valores.put("velocidad", sinVelocidad);
+            }
+            //BBDD nueva rellenandose.
+            if(sobreescribir){
+                //Si se ha reiniciado el puntero de la BD, se han de actualizar valores, no introducirlos.
+                salida=db.update("posiciones", valores, "id=" + id,null);
+            } else{
+                salida=db.insert("posiciones", null, valores);
+            }
+            if(id<indiceMaximo){
+                id=id+1;            //Actualizamos al indice para en siguiente dato.
+            } else {
+                id=0;
+                sobreescribir=true;
+            }
+        }
+        db.close();
+        return(salida>0);
+    }
+
+    public boolean  borrarLocalizacion(int id) {
+        SQLiteDatabase db = getWritableDatabase();
+        long salida=0;
+        if (db != null) {
+            salida=db.delete("contactos", "_id=" + id, null);
+        }
+        db.close();
+        return(salida>0);
+    }
+
+    public ArrayList<Point> listaPosiciones(){
+        SQLiteDatabase db = getReadableDatabase();
+        //Se leen todas las posiciones existentes en la BBDD, y se crean los objetos point donde devolverlas.
+        ArrayList<Point> localizaciones= new ArrayList<Point>();
+        String[] valores_recuperar = {"latitud", "longitud", "velocidad", "tiempo"};
+        int puntero=id;
+        int indice=0;
+        if(db!=null){
+            Cursor c=db.query("posiciones", valores_recuperar, "id=" + id, null, null, null, null, null);
+            c.move(puntero);
+            do{
+                Point punto;
+                Location localizacion=new Location("puntoA");
+                localizacion.setLatitude(Location.convert(c.getString(0)));
+                localizacion.setLongitude(Location.convert(c.getString(2)));
+                localizacion.setSpeed(Float.parseFloat(c.getString(3)));
+                localizacion.setTime(Long.parseLong(c.getString(4)));
+                if(indice==0){
+                    //Si el el primer punto, no puede sacarse una distancia recorrida ni una aceleracion.
+                    punto=new Point(indice, localizacion.getLatitude(),localizacion.getLongitude(),0,localizacion.getSpeed(),0);
+                } else {
+                    //Se necesita acceder al punto anterior para conseguir la distancia y la aceleracion.
+                    c.moveToPrevious();
+                    Location localizacion2=new Location("puntoB");
+                    localizacion2.setLatitude(Location.convert(c.getString(0)));
+                    localizacion2.setLongitude(Location.convert(c.getString(2)));
+                    localizacion2.setSpeed(Float.parseFloat(c.getString(3)));
+                    localizacion2.setTime(Long.parseLong(c.getString(4)));
+                    punto=new Point(indice, localizacion.getLatitude(),localizacion.getLongitude(),
+                            localizacion.distanceTo(localizacion2),localizacion.getSpeed(),
+                            (double)((localizacion.getSpeed()-localizacion2.getSpeed())/(localizacion2.getTime()-localizacion.getTime())));
+                            //Formula comun de la aceleracion: A=(V1-V0)/T
+                    c.moveToNext(); //Se vuelve a situar el cursor donde estaba.
+                }
+                indice=indice +1;
+                localizaciones.add(punto);
+            } while (c.moveToNext());
+
+            //Tambien es necesario anadir los puntos que se llevan sobreescritos en la pasada actual por la BD.
+            for (puntero=0; puntero<id; puntero++ ){
+                c.move(puntero);
+                Point punto;
+                if(puntero==0) {
+                    Location localizacion = new Location("puntoA");
+                    localizacion.setLatitude(Location.convert(c.getString(0)));
+                    localizacion.setLongitude(Location.convert(c.getString(2)));
+                    localizacion.setSpeed(Float.parseFloat(c.getString(3)));
+                    localizacion.setTime(Long.parseLong(c.getString(4)));
+                    if(id!=0){
+                        c.moveToLast();
+                        Location localizacion2=new Location("puntoB");
+                        localizacion2.setLatitude(Location.convert(c.getString(0)));
+                        localizacion2.setLongitude(Location.convert(c.getString(2)));
+                        localizacion2.setSpeed(Float.parseFloat(c.getString(3)));
+                        localizacion2.setTime(Long.parseLong(c.getString(4)));
+                        punto=new Point(indice, localizacion.getLatitude(),localizacion.getLongitude(),
+                                localizacion.distanceTo(localizacion2),localizacion.getSpeed(),
+                                (double)((localizacion.getSpeed()-localizacion2.getSpeed())/(localizacion2.getTime()-localizacion.getTime())));
+                        //Formula comun de la aceleracion: A=(V1-V0)/T
+                    } else {
+                        punto=new Point(indice, localizacion.getLatitude(),localizacion.getLongitude(),0,localizacion.getSpeed(),0);
+                    }
+                    c.move(puntero);
+                } else {
+                    Location localizacion = new Location("puntoA");
+                    localizacion.setLatitude(Location.convert(c.getString(0)));
+                    localizacion.setLongitude(Location.convert(c.getString(2)));
+                    localizacion.setSpeed(Float.parseFloat(c.getString(3)));
+                    localizacion.setTime(Long.parseLong(c.getString(4)));
+
+                    Location localizacion2=new Location("puntoB");
+                    localizacion2.setLatitude(Location.convert(c.getString(0)));
+                    localizacion2.setLongitude(Location.convert(c.getString(2)));
+                    localizacion2.setSpeed(Float.parseFloat(c.getString(3)));
+                    localizacion2.setTime(Long.parseLong(c.getString(4)));
+
+                    punto=new Point(indice, localizacion.getLatitude(),localizacion.getLongitude(),
+                            localizacion.distanceTo(localizacion2),localizacion.getSpeed(),
+                            (double)((localizacion.getSpeed()-localizacion2.getSpeed())/(localizacion2.getTime()-localizacion.getTime())));
+                    //Formula comun de la aceleracion: A=(V1-V0)/T
+                }
+                localizaciones.add(punto);
+            }
+            db.close();
+            c.close();
+        }
+        return localizaciones;
+    }
+}
--- app/src/main/java/com/dam/proyectodam/CalculationActivity.java
+++ app/src/main/java/com/dam/proyectodam/CalculationActivity.java
@@ -1,7 +1,11 @@
 package com.dam.proyectodam;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -20,6 +24,13 @@ import android.widget.Toast;
  *
  */
 public class CalculationActivity extends Activity {
+
+    //Interesa que esten aqui porque van a ser accedidas desde diferentes metodos.
+    private BBDD baseDatos;
+    private static final int TIEMPO_ACTUALIZACION = 1000 * 20;      //Veinte segundos
+    private static final int DISTANCIA_ACTUALIZACION= 10;           //Diez metros.
+    Location ultima_localizacion;
+
 
     /**
      * Método: onCreate
@@ -60,5 +71,115 @@ public class CalculationActivity extends Activity {
         // Y cerramos la actividad para no volver a ella al pulsar atrás.
         finish();
     }
+
+    //Metido qe inicia el GPS (y con el la BBDD).
+    public void iniciarGPS(){
+        // Referencia al gestor de localizacion del sistema.
+        //Usando eso el movil usara tanto el GPS como las antenas de telefonia para posicionarse.
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        //Tambien se inicia la base de datos.
+        baseDatos=new BBDD(getApplicationContext());
+
+        //Para no hacer esperar demasiado al usuario se le muestra la ultima localizacion conocida mientras se busca la actual.
+        ultima_localizacion = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        //Este punto no veo necesario guardarlo en la bd.
+        //INSERTAR AQUI PARA ENVIARLO AL MAPA.
+
+        //Clase Locationlistener, llevar los metodos que son llamados al iniciar apagaga y actualizar los datos de posicion.
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //Esto es lo principal, registrar cambios, actualizar posicion en el mapa y guardarla en la base de datos.
+                //La linea siguiente deberia centrar el mapa en la posicion adquirica. Sustituir "mapa" por la referencia al mapa.
+                // mapa.setCenter(new GeoPoint((int)location.getLatitude(),(int)location.getLongitude()))   //Cuando se añada la API del Maps
+
+
+                //Comprobamos que merece la pena actualizar la posicion. Si no, se ignora y se espera a la siguiente.
+                if(isBetterLocation(location, ultima_localizacion)){
+                    //Con esto se guarda la posicion en la BD
+                    baseDatos.insertarPosicion(location);
+                }
+
+                //Si se quiere usar la posicion sin pasar por la base de datos, hacerlo desde aqui.
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        //Actualizar usando GPS (lanzar LocationCahnged). Minimo cada 5 segundos o 10 metros.
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TIEMPO_ACTUALIZACION, DISTANCIA_ACTUALIZACION, listener );
+    }
+
+
+    //Logica molona que determina si es conveniente usar la nueva localizacion o esperarse a otra.
+    //Mejorar para que tenga tambien en cuenta la distancia.
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TIEMPO_ACTUALIZACION;
+        boolean isSignificantlyOlder = timeDelta < -TIEMPO_ACTUALIZACION;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
+
 
 }
